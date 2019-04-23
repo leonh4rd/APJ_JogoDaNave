3efb6a543ab9c2716f7d8437ea2eb3214f0da128
Leonardo (Notebook) <lnrd.ferreira@hotmail.com>
2019-04-23 17:26:20 -0300

    Add ship controller

---
+++ Assets/Prefabs/Ship/ShipController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get input
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        //Move ship
+        transform.Translate(new Vector3(horizontal, 0f, 0f));
+    }
+}
