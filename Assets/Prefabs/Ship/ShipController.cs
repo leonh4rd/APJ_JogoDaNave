ce30f55cbba01912ce873370cf1a3a89fafb1f0b
Leonardo (Notebook) <lnrd.ferreira@hotmail.com>
2019-04-23 17:46:13 -0300

    Add shoot capacity to ship

--- Assets/Prefabs/Ship/ShipController.cs
+++ Assets/Prefabs/Ship/ShipController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    public Transform shootPrefab = null;
     public float speed = 1.0f;
 
     // Start is called before the first frame update
@@ -20,5 +21,18 @@ public class ShipController : MonoBehaviour
 
         //Move ship
         transform.Translate(new Vector3(horizontal, 0f, 0f));
+
+        //Shooting
+        if(Input.GetButtonDown("Fire1"))
+        {
+            //Create new shoot object
+            Transform newshoot = Instantiate<Transform>(shootPrefab);
+
+            //Position shoot object
+            newshoot.position = transform.position;
+
+            //Set shoot speed
+            newshoot.GetComponent<ShootController>().speed = 30.0f;
+        }
     }
 }
--- Assets/Prefabs/Shoot/ShootController.cs
+++ Assets/Prefabs/Shoot/ShootController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ShootController : MonoBehaviour
 {
+    public float speed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class ShootController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(0f, speed * Time.deltaTime, 0f));
+
+        //When shoot out of screen
+        if(transform.position.y > 100f || transform.position.y <= -100f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
